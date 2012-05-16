e7139b0d4633f810407755261103f1bae7ef4480
JF Ding <jian-feng.ding@intel.com>
2012-05-16 17:55:48 +0800

    update setup.py to apply VERSION in installation

--- setup.py
+++ setup.py
@@ -8,8 +8,28 @@ try:
 except ImportError:
     pass
 
+MOD_NAME = 'kickstart'
+
+version_path = 'VERSION'
+if not os.path.isfile(version_path):
+    print 'No VERSION file in topdir, abort'
+    sys.exit(1)
+
+try:
+    # first line should be the version number
+    version = open(version_path).readline().strip()
+    if not version:
+        print 'VERSION file is invalid, abort'
+        sys.exit(1)
+
+    ver_file = open('%s/__version__.py' % MOD_NAME, 'w')
+    ver_file.write("VERSION = \"%s\"\n" % version)
+    ver_file.close()
+except IOError:
+    print 'WARNING: Cannot write version number file'
+
 setup(name='kickstarter',
-      version = "0.2",
+      version = version,
       description='Kickstarter',
       author='Anas Nashif',
       author_email='anas.nashif@intel.com',
